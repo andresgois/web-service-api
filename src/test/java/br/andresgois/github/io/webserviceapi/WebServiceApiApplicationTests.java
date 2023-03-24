b34c9a81dd3382a616a229b91fff1a772455800f
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-03-24 19:20:25 -0300

    :tada: init: first commit

---
+++ src/test/java/br/andresgois/github/io/webserviceapi/WebServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.andresgois.github.io.webserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
