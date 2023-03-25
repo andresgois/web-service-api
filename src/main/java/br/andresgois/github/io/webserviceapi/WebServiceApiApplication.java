a1ff68a0252eaf05cc0665c6f14e7a68f7e9b8ff
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-03-24 21:39:23 -0300

    :test_tube:	test: teste do servico client

--- src/main/java/br/andresgois/github/io/webserviceapi/WebServiceApiApplication.java
+++ src/main/java/br/andresgois/github/io/webserviceapi/WebServiceApiApplication.java
@@ -1,10 +1,28 @@
 package br.andresgois.github.io.webserviceapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import br.andresgois.github.io.webserviceapi.service.RestService;
 
 @SpringBootApplication
 public class WebServiceApiApplication {
+    
+    @Autowired 
+    RestService service;
+    
+    @Bean
+    public CommandLineRunner init(RestService service) {
+        
+        return args -> {
+            String s = service.getService();
+            System.out.println(s);
+        };
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebServiceApiApplication.class, args);
