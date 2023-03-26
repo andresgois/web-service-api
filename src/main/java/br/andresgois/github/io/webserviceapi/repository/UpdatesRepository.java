1d32b181d61658a6ae0b743777ef50f5a18b7ebe
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-03-25 21:28:52 -0300

    :sparkles:	feat: classe dos repositories

---
+++ src/main/java/br/andresgois/github/io/webserviceapi/repository/UpdatesRepository.java
@@ -0,0 +1,12 @@
+package br.andresgois.github.io.webserviceapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.andresgois.github.io.webserviceapi.model.Updates;
+
+@Repository
+public interface UpdatesRepository extends JpaRepository<Updates, String> {
+    
+    Boolean existsByIdObj(String idObj);
+}
