1d63483a240f4422399ed35ecf48be7071942f9b
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-03-25 21:27:35 -0300

    :wrench:	chore: configiracoes de testes iniciais

--- src/main/java/br/andresgois/github/io/webserviceapi/WebServiceApiApplication.java
+++ src/main/java/br/andresgois/github/io/webserviceapi/WebServiceApiApplication.java
@@ -5,22 +5,26 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import br.andresgois.github.io.webserviceapi.service.RestService;
+import br.andresgois.github.io.webserviceapi.service.UpdateService;
 
 @SpringBootApplication
 public class WebServiceApiApplication {
     
     @Autowired 
-    RestService service;
+    private RestService service;
+    
+    @Autowired
+    private UpdateService updateService;
     
     @Bean
     public CommandLineRunner init(RestService service) {
         
         return args -> {
-            String s = service.getService();
-            System.out.println(s);
+            updateService.inserirDadosExternos();
+            /*String s = service.getService();
+            System.out.println(s);*/
         };
     }
 
