a74595a775240714f82ca4f9b863cd601e6525a1
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-03-25 21:26:30 -0300

    :wrench:	chore: classe de contantes

--- src/main/java/br/andresgois/github/io/webserviceapi/util/Contants.java
+++ src/main/java/br/andresgois/github/io/webserviceapi/util/Contants.java
@@ -5,4 +5,5 @@ public final class Contants {
     private Contants() {}
     
     public static final String ERRO_CONVERSAO = "Erro ao converter string para objeto";
+    public static final String SALVO_SUCESSO = "SALVO COM SUCESSO, ID: ";
 }
